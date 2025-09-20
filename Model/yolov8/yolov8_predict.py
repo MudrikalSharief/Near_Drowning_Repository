00a24e74d6310b5f436daa8a450a00c53b99e2b1
Mudrikal, Sharief kundo L. <shariefkundo@gmail.com>
2025-09-20 10:56:26 +0800

    Update : the output of video is tailored to output_video name

--- Model/yolov8/yolov8_predict.py
+++ Model/yolov8/yolov8_predict.py
@@ -1,21 +1,28 @@
 import cv2
 from ultralytics import YOLO
-
+import os 
 # Load the YOLO model
 model = YOLO("C:/Users/Lenovo Slim i3/Documents/GitHub/Near_Drowning_Repository/Model/yolov8/PD_default.pt")
 
 # Open the video file
 video_path = "C:/Users/Lenovo Slim i3/Documents/GitHub/Near_Drowning_Repository/Model/yolov8/test_video/test2.mp4"
 cap = cv2.VideoCapture(video_path)
+video_name = os.path.basename(video_path)
+print("Video name:", video_name)
 
 # Optional: set output video writer if you want to save results
 save_output = True
 if save_output:
+    output_dir = 'C:/Users/Lenovo Slim i3/Documents/GitHub/Near_Drowning_Repository/Model/yolov8/output_videos'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    out = cv2.VideoWriter("output.mp4", fourcc, fps, (width, height))
+    output_path = os.path.join(output_dir, f"output_{video_name}")
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
 
 # Loop through the video frames
 while cap.isOpened():
